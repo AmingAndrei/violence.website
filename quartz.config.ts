50a16ef5fc8a7f8ebac56286a355ce42ffe6b051
Aming <andreigurka67@gmail.com>
2025-05-12 00:21:07 +0300

    Quartz sync: May 12, 2025, 12:21 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "",
+    pageTitle: "Acheron Database",
     pageTitleSuffix: "Acheron Database",
     enableSPA: true,
     enablePopovers: true,
