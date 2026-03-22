00afbde8ae2d7c89d0b6315593419ba9fb2da12f
AmingAndrei <andreigurka67@gmail.com>
2026-03-22 22:58:38 +0200

    Quartz sync: Mar 22, 2026, 10:58 PM

--- quartz/components/Logo.tsx
+++ quartz/components/Logo.tsx
@@ -1,33 +1,27 @@
-import { QuartzComponent, QuartzComponentConstructor } from "./types"
-
-const Logo: QuartzComponent = () => {
-  return (
-    <div class="site-logo">
-      <img src="/logo.png" alt="VIOLENCE" />
-    </div>
-  )
-}
-
-Logo.css = `
-.site-logo {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-
-.site-logo img {
-  height: 117px;
-  width: auto;
-  max-height: none;
-}
-`
+import { QuartzComponentConstructor } from "./types"
 
 export default (() => {
-  return () => (
+  const Logo = () => (
     <div class="site-logo">
       <a href="/">
-        <img src="static/logo.png" alt="VIOLENCE" />
+        <img src="/static/logo.png" alt="VIOLENCE" />
       </a>
     </div>
   )
+
+  Logo.css = `
+    .site-logo {
+      display: flex;
+      justify-content: center;
+      width: 100%;
+    }
+
+    .site-logo img {
+      height: 117px;
+      width: auto;
+      max-height: none;
+    }
+  `
+
+  return Logo
 }) satisfies QuartzComponentConstructor
