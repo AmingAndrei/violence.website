9297073a898eef5116401f4b3cf0c91e204fa8a7
AmingAndrei <andreigurka67@gmail.com>
2026-04-01 13:34:17 +0300

    Quartz sync: Apr 1, 2026, 1:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "violence.website.pages.dev",
+    baseUrl: "acheron-database.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
